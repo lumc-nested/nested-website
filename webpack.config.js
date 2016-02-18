9623caae429c0c153ddeea8d168360440a5cd540
Martijn Vermaat <martijn@vermaat.name>
2016-02-18 18:03:59 +0100

    Combine dev and dist webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 'use strict';
 
 
+var path = require('path');
 var webpack = require('webpack');
 
 
-module.exports = {
+// Detect if we're running webpack dev server or building a distribution.
+var devServer = path.basename(require.main.filename) === 'webpack-dev-server.js';
+
+
+var config = {
   plugins: [
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({compress: {drop_console: true}}),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin()
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
   ],
 
   entry: './scripts/init.jsx',
@@ -69,3 +70,35 @@ module.exports = {
   debug: false,
   devtool: false
 };
+
+
+if (devServer) {
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({'process.env.NODE_ENV': '"development"'})
+  );
+  config.entry = [
+    'webpack/hot/only-dev-server',
+    './scripts/init.jsx'
+  ];
+  config.devServer = {
+    hot: true
+  };
+  config.output.publicPath = '/dist/';
+  config.module.loaders[0].loader = 'react-hot!babel-loader?stage=1&optional=runtime';
+  config.cache = true;
+  config.debug = true;
+  config.devtool = 'eval';
+} else {
+  config.plugins.push(
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({compress: {drop_console: false}}),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'})
+  );
+}
+
+
+module.exports = config;
